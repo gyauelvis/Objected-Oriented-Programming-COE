0f6b4618efa7307feb21db1641f28915f803d56a
Gyau Boahen Elvis <66832781+gyauelvis@users.noreply.github.com>
2023-07-25 04:40:23 +0000

    Update semester_project.cpp

--- personal/semester_project.cpp
+++ personal/semester_project.cpp
@@ -1,9 +1,7 @@
 #include <iostream>
 #include "main.h"
-#include <iomanip>
-#include <string>
-
 using namespace std;
+
 
 int main()
 {
@@ -13,54 +11,60 @@ int main()
     int choice;
     string username;
     string password;
+    bool returnToMainMenu = false; // Variable to track whether to return to the main menu
+
     //Styling the main menu page
-    cout<<"\t\t\t\t\t____________________________________\n";
-    cout<<"\t\t\t\t\t                                     \n";
-    cout<<"\t\t\t\t\t                                  \n";
-    cout<<"\t\t\t\t\t    SUPERMARKET MAIN MENU         \n";
-    cout<<"\t\t\t\t\t                                   \n";
-    cout<<"\t\t\t\t\t____________________________________\n";
-    cout<<"\t\t\t\t\t                                     \n";
-    cout<<"\t\t\t\t\t      |    [1] ADMIN    |\n";
-    cout<<"\t\t\t\t\t      |                 |\n";
-    cout<<"\t\t\t\t\t      |    [2] BUYER    |\n";
-    cout<<"\t\t\t\t\t      |                 |\n";
-    cout<<"\t\t\t\t\t      |    [3] EXIT     |\n";
-    cout<<"\t\t\t\t\t                                      \n";
-    cout<<"\n\t\t\t\tPlease select: ";
-    cin >>choice;
+    cout << "\n\t\t\t\t\t____________________________________\n";
+    cout << "\t\t\t\t\t                                     \n";
+    cout << "\t\t\t\t\t                                  \n";
+    cout << "\t\t\t\t\t       SUPERMARKET MAIN MENU         \n";
+    cout << "\t\t\t\t\t                                   \n";
+    cout << "\t\t\t\t\t____________________________________\n";
+    cout << "\t\t\t\t\t                                     \n";
+    cout << "\t\t\t\t\t      |    [1] ADMIN    |\n";
+    cout << "\t\t\t\t\t      |                 |\n";
+    cout << "\t\t\t\t\t      |    [2] CUSTOMER |\n";
+    cout << "\t\t\t\t\t      |                 |\n";
+    cout << "\t\t\t\t\t      |    [3] EXIT     |\n";
+    cout << "\t\t\t\t\t                                      \n";
+    cout << "\n\t\t\t\tPlease select an option: ";
+    cin >> choice;
     cout<<"\n";
     switch(choice)
     {
     case 1:
-        cout<<"\t\t\t\t\t---------Login Page--------------\n";
-        cout<<"\n\t\t\tusername: ";
-        cin>>username;
-        cout<<"\n\n\t\t\tPassword: ";
-        cin>>password;
+        cout<<"\t\t\t\t" << "___________________________________________________\n";
+        cout<<"\n";
+        cout <<"\t\t\t\t"<< "\t\t\t  Login Page\n";
+        cout<<"\t\t\t\t" << "___________________________________________________\n";
+        cout << "\n\t\t\tusername: ";
+        cin >> username;
+        cout << "\n\t\t\tPassword: ";
+        cin >> password;
 
         if (username=="admin" && password=="password"){
-           shop1.admin();
+            returnToMainMenu = shop1.admin(); // Store the return value from admin() in the variable
         }
         else
         {
-            cerr<<"Invalid email/password";
+            cerr<<"\t\t\tInvalid email/password"<<endl;
         }
         break;
     case 2:
-            shop1_customer.bmenu();
-            if(shop1_customer.bmenu() == false){
-                system("cls");
-                goto main_menu;
-            }
-            break;
+        returnToMainMenu = shop1_customer.bmenu(); // Store the return value from bmenu() in the variable
+        break;
     case 3:
-            cout<<"\t\tThanks for shopping with us"<<endl;
-            exit(0);
-            break;
+        cout<<"\t\t\t\tThanks for shopping with us"<<endl;
+        exit(0);
+        break;
     default:
-            cout<<"Please select from the given options";
-            goto main_menu;
+        cout<<"Please select from the given options";
+        goto main_menu;
+    }
+
+    if (shop1.admin()) {
+        system("cls");
+        goto main_menu; // Go back to the main menu if the return value is true
     }
 
     return 0;
